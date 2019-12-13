0a740e404fd29e606ce087cce2a89560deb29386
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-12-12 23:11:58 -0600

    myName

--- JS/functions.js
+++ JS/functions.js
@@ -36,6 +36,9 @@
  * console.
  */
 
+    let myName = "Luna";
+    console.log(sayHello(myName));
+
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
