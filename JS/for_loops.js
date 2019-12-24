55d0e49a39843ae164b85623fc0f5b00d5a5bb6c
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-12-24 15:39:12 -0600

    for loop multiple exercise

---
+++ JS/for_loops.js
@@ -0,0 +1,10 @@
+ console.log('hi');
+
+const showMultiplicationTable = (num) => {
+
+    for(let i = 0; i <= 10; i++){
+        mult = (num * i);
+    console.log(num + 'x' + i + '=' + mult)
+    }
+    };
+showMultiplicationTable(5);
