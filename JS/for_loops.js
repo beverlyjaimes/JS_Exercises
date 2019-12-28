7f33b00aadd11c3d74c43f6d00e00ec9102adfa7
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-12-27 18:45:39 -0600

    working on forked branch coffee project

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,10 +1,13 @@
  console.log('hi');
 
-const showMultiplicationTable = (num) => {
+// const showMultiplicationTable = (num) => {
+ // //
+ // //     for(let i = 0; i <= 10; i++){
+ // //         mult = (num * i);
+ // //     console.log(num + 'x' + i + '=' + mult)
+ // //     }
+ // //     };
+ // // showMultiplicationTable(5);
 
-    for(let i = 0; i <= 10; i++){
-        mult = (num * i);
-    console.log(num + 'x' + i + '=' + mult)
-    }
-    };
-showMultiplicationTable(5);
+ // ENTRY-LEVEL JOB TITLES
+ // Web Programmer, Web Developer, Web Applications Developer, Software Developer, Computer Programmer, Quality Assurance Engineer, Front-End Developer.
