a84fb8940cc9d8d4c6fb7c2132ab6d9316916707
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-04 14:14:08 -0600

    exercise one finished

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,2 +1,10 @@
 // prompt the user for an odd number between 1 and 50
 // (use a loop if they to continue to ask for a number if until it is odd)
+
+let userNum = prompt("Enter an odd number: ");
+
+if(userNum  % 2 ){
+    alert("Thanks!")
+} else {
+    alert("That is not an odd number!")
+}
