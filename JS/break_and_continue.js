6391d943af0e747b99c273eb3c5fea0b42eedd4a
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-07 19:23:14 -0600

    new file to save bonus pracitce problems

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,17 +1,33 @@
 // prompt the user for an odd number between 1 and 50
 // (use a loop if they to continue to ask for a number if until it is odd)
+//
+// let userNum = prompt("Enter an odd number between 1 and 50: ");
+//
+// for (var i = 0; i <= 50 ; i++){
+//     alert("your number is " + userNum);
+//
+//     if(i % 2 ){
+//         alert("thanks");
+//     break;
+//
+//     }  else if (userNum > 50) {
+//         prompt("that number is greater than 50 or even")
+//     }
 
-let userNum = prompt("Enter an odd number between 1 and 50: ");
+// }
 
-for (var i = 0; i <= 50 ; i++){
-    alert("your number is " + userNum);
 
-    if(i % 2 ){
-        alert("thanks");
-    break;
+// displayMessage();
+//
+// function displayMessage() {
+//     console.log(message);
+// }
+//
+// var message = "Hello Codeup!";
+//
 
-    }  else if (userNum > 50) {
-        prompt("that number is greater than 50 or even")
-    }
+var parks = ["Arches", "Badlands", "Carlsbad", "Denali"];
 
+for(var i = 0; i < parks.length ; i++) {
+    console.log(parks[i]);
 }
