3aba12804a46d5bd11841c068799db24e9ca7395
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-07 19:26:43 -0600

    exercise 3

--- JS/codingjs_bonus.js
+++ JS/codingjs_bonus.js
@@ -34,3 +34,21 @@ function monkeyTrouble(aSmile, bSmile){
         return false;
     }
 }
+
+// Warmup-1 -- sumDouble
+// Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+//
+//     Examples
+//
+// sumDouble(1, 2) → 3
+// sumDouble(3, 2) → 5
+// sumDouble(2, 2) → 8
+
+function sumDouble(a, b){
+    var sum = a + b
+    if (a === b){
+        return sum * 2
+    } else {
+        return sum;
+    }
+}
