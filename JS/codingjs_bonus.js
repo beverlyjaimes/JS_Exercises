1a74ce186207b35523e977b00ab5716389680a76
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-08 21:37:59 -0600

    exercise 8

--- JS/codingjs_bonus.js
+++ JS/codingjs_bonus.js
@@ -127,3 +127,22 @@ function nearHundred(n){
     }
 
 }
+
+//Warmup-1 -- notString
+// Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+//
+// Examples
+//
+// notString('candy') → not candy
+// notString('x') → not x
+// notString('not bad') → not bad
+
+function notString(str){
+    let notIncludes = str.startsWith("not");
+
+    if(notIncludes) {
+        return str
+    } else {
+        return 'not ' + str
+    }
+}
