8b040b1a9bdb4f121eead27d23d070d4802bb9ce
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-08 21:37:26 -0600

    exercise 7

--- JS/codingjs_bonus.js
+++ JS/codingjs_bonus.js
@@ -109,3 +109,21 @@ function makes10(a, b){
     }
 
 }
+
+// Warmup-1 -- nearHundred
+// Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+//
+//     Examples
+//
+// nearHundred(93) → true
+// nearHundred(90) → true
+// nearHundred(89) → false
+
+function nearHundred(n){
+    if((Math.abs(n) >= 90 && Math.abs(n) <=99) || (Math.abs(n) >= 190 || Math.abs(n) >=199) || (Math.abs(n) >= 101 && Math.abs(n) <=110) || (Math.abs(n) >= 201 || Math.abs(n) >= 210)){
+        return true;
+    } else {
+        return false;
+    }
+
+}
