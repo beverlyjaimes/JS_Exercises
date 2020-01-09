9393a181deb3729e06541c32cf1207e428e32227
beverlyjaimes <beverly.jaimespuente@gmail.com>
2020-01-08 21:36:50 -0600

    exercise 6

--- JS/codingjs_bonus.js
+++ JS/codingjs_bonus.js
@@ -89,3 +89,23 @@ function parrotTrouble(talking, hour){
     }else{
         return false}
 }
+
+// Warmup-1 -- makes10
+// Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+//
+// Examples
+//
+// makes10(9, 10) → true
+// makes10(9, 9) → false
+// makes10(1, 9) → true
+
+function makes10(a, b){
+    if(a === 10 || b === 10){
+        return true;
+    }else if(a + b === 10){
+        return true;
+    } else{
+        return false;
+    }
+
+}
